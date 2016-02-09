52e70fbfcc35e04518f56bf700bdfabca047bd56
Jalg <josealarrubiag121292@hotmail.com>
2016-02-09 22:06:07 +0100

    Añadidos comentarios.
    Añadidos comentarios a todas las funciones y clases.

--- app/src/main/java/com/example/josea/puntogestosfoto/CamTestActivity.java
+++ app/src/main/java/com/example/josea/puntogestosfoto/CamTestActivity.java
@@ -1,17 +1,16 @@
 package com.example.josea.puntogestosfoto;
 
-
-//Documentacion usada:
-// Parte 2.5  y 3 de : http://nuevos-paradigmas-de-interaccion.wikispaces.com/Detecci%C3%B3n+de+patrones+en+Android+-+Gesture+Builder
-//https://github.com/josnidhin/Android-Camera-Example
-//http://nosoandroid.blogspot.com.es/2013/02/retardar-la-ejecucion-de-codigo-durante.html
+/*
+Autores:
+    José Miguel Navarro Moreno
+    José Antonio Larrubia García
+*/
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
 
 import android.os.Handler;
 import android.app.Activity;
@@ -33,45 +32,80 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 
+/// <summary>
+/// Clase principal de la aplicación, cámara de fotos que hará una foto a los 3 segundos de reconocer el patrón dibujado en pantalla adecuado.
+/// Se ha usado la siguiente documentación:
+/// Parte 2.5  y 3 de : http://nuevos-paradigmas-de-interaccion.wikispaces.com/Detecci%C3%B3n+de+patrones+en+Android+-+Gesture+Builder
+/// para la realización de la parte del patrón.
+/// https://github.com/josnidhin/Android-Camera-Example
+/// como base para la cámara.
+/// </summary>
 public class CamTestActivity extends Activity implements GestureOverlayView.OnGesturePerformedListener {
+
+    /// <summary>
+    /// Variable usada para los mensajes de depuración.
+    /// </summary>
     private static final String TAG = "CamTestActivity";
+
+    /// <summary>
+    /// Estructura que usa la cámara para poder usarla sin problema, ver su archivo para más información.
+    /// </summary>
     Preview preview;
-    Button buttonClick;
+
+    /// <summary>
+    /// Sensor de la cámara.
+    /// </summary>
     Camera camera;
+
+    /// <summary>
+    /// Estructura para la actividad, asignada por la superclase.
+    /// </summary>
     Activity act;
+
+    /// <summary>
+    /// Estructura para el contexto de la aplicación, asignada por la superclase.
+    /// </summary>
     Context ctx;
 
-    //Variable para los gestos
+    /// <summary>
+    /// Estructura para los gestos.
+    /// </summary>
     private GestureLibrary gestureLib;
 
-
+    /// <summary>
+    /// Función que se llama al iniciar la aplicación, inicia todos los servicios necesarios.
+    /// </summary>
+    /// <param name=" savedInstanceState "> Parámetro con la instancia de la aplicación, pasado por android </param>
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ctx = this;
         act = this;
+
+        //Ponemos que no nos aparezca el titulo de la aplicación en el layout.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.main);
 
+        // Iniciamos lo que se requiere para los gestos, importante iniciarlos antes que los de la cámara.
         PrepararGestos();
 
-
+        // Iniciamos lo necesario para la cámara usando la clase Preview.
         preview = new Preview(this, (SurfaceView)findViewById(R.id.surfaceView));
         preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         ((FrameLayout) findViewById(R.id.layout)).addView(preview);
         preview.setKeepScreenOn(true);
-
-        Toast.makeText(ctx, getString(R.string.take_photo_help), Toast.LENGTH_LONG).show();
     }
 
+    /// <summary>
+    /// Función llamada por android al reanudar la aplicación después de estar en pausa para recuperarse.
+    /// </summary>
     @Override
     protected void onResume() {
         super.onResume();
@@ -87,6 +121,9 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
         }
     }
 
+    /// <summary>
+    /// Función llamada cuando se pause la aplicación.
+    /// </summary>
     @Override
     protected void onPause() {
         if(camera != null) {
@@ -95,35 +132,48 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
             camera.release();
             camera = null;
         }
-        else{
-            System.exit(0);
-        }
         super.onPause();
     }
 
+    /// <summary>
+    /// Función que reinicia la cámara después de realizar una foto.
+    /// </summary>
+    /// <param name=" ">  </param>
     private void resetCam() {
         camera.startPreview();
         preview.setCamera(camera);
     }
 
+    /// <summary>
+    /// Función para actualizar la galeria de fotos del dispositivo.
+    /// </summary>
+    /// <param name=" file "> fichero con el que se actualizará la galería </param>
     private void refreshGallery(File file) {
         Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         mediaScanIntent.setData(Uri.fromFile(file));
         sendBroadcast(mediaScanIntent);
     }
 
+    /// <summary>
+    /// Estructura necesaria para la cámara cuando se realiza la foto.
+    /// </summary>
     ShutterCallback shutterCallback = new ShutterCallback() {
         public void onShutter() {
-            //			 Log.d(TAG, "onShutter'd");
         }
     };
 
+    /// <summary>
+    /// Estructura necesaria para la cámara cuando se realiza la foto.
+    /// </summary>
     PictureCallback rawCallback = new PictureCallback() {
         public void onPictureTaken(byte[] data, Camera camera) {
-            //			 Log.d(TAG, "onPictureTaken - raw");
         }
     };
 
+    /// <summary>
+    /// Estructura necesaria para la cámara cuando se realiza la foto, en esta si hacemos algo más que en las anteriores,
+    /// guardamos la imagen correctamente y reiniciamos la cámara.
+    /// </summary>
     PictureCallback jpegCallback = new PictureCallback() {
         public void onPictureTaken(byte[] data, Camera camera) {
             new SaveImageTask().execute(data);
@@ -132,21 +182,33 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
         }
     };
 
+    /// <summary>
+    /// Clase para guardar correctamente la imagen.
+    /// </summary>
     private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
 
+        /// <summary>
+        /// Función para que al hacer la foto la almacene correctamente.
+        /// </summary>
+        /// <param name=" data "> parámetro que contiene los datos de la imagen </param>
         @Override
         protected Void doInBackground(byte[]... data) {
+            // Inicializamos el flujo de salida.
             FileOutputStream outStream = null;
 
-            // Write to SD Card
+            // guardamos la imagen en la sd.
             try {
                 File sdCard = Environment.getExternalStorageDirectory();
+
+                //Si no existe el directorio donde se almacenan las fotos de la aplicación se crea
                 File dir = new File (sdCard.getAbsolutePath() + "/app_gesto_foto");
                 dir.mkdirs();
 
+                //Iniciamos el nombre que tendra el fichero y lo creamos.
                 String fileName = String.format("%d.jpg", System.currentTimeMillis());
                 File outFile = new File(dir, fileName);
 
+                //Guardamos el flujo en en el fichero de salida.
                 outStream = new FileOutputStream(outFile);
                 outStream.write(data[0]);
                 outStream.flush();
@@ -154,6 +216,7 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
 
                 Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
 
+                //Actualizamos la galeria con el nuevo fichero.
                 refreshGallery(outFile);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -166,6 +229,9 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
 
     }
 
+    /// <summary>
+    /// Función llamada por onCreate para inicializar correctamente lo necesario para el analizador de gestos.
+    /// </summary>
     public void PrepararGestos(){
         GestureOverlayView gestureOverlayView = new GestureOverlayView(this);
         View inflate = getLayoutInflater().inflate(R.layout.main, null);
@@ -179,26 +245,34 @@ public class CamTestActivity extends Activity implements GestureOverlayView.OnGe
         setContentView(gestureOverlayView);
     }
 
+    /// <summary>
+    /// Función que detecta si el patrón que se realiza en pantalla coincide con alguno de los almacenados.
+    /// </summary>
+    /// <param name=" overlay "> parámetro para leer desde pantalla el gesto, necesario para el reconocimiento aunque no lo usemos concretamente </param>
+    /// <param name=" gesture "> parámetro que almacena el gesto realizado en pantalla por el usuario  </param>
     public void onGesturePerformed (GestureOverlayView overlay, Gesture gesture){
         //Manejador para hacer que tome la foto en 3 segundos
         Handler mhandler = new Handler();
+
+        //Lista que guarda lo parecido que es el patrón hecho en pantalla por el usuario con el almacenado por la aplicación.
         ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
 
         for (Prediction prediction : predictions) {
+            // Comprobamos si alguno de los gestos introducidos por pantalla es parecido al nuestro.
+            // Hemos elegido un score de 4 porque ni era lo suficiente sensible para que no detectará ningún gesto como parecido ni
+            // lo suficiente tosco para que detectara cualquier patrón como si fuera el nuestro, calculado haciendo pruebas.
             if (prediction.score > 4.0) {
-
+                //Mostramos un mensaje en pantalla
                 Toast.makeText(this,getString(R.string.Foto_en_3) , Toast.LENGTH_SHORT).show();
 
+                //Hacemos la foto cuando pasan 3 segundos.
                 mhandler.postDelayed(new Runnable() {
                     public void run() {
                         camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-                        Toast.makeText(ctx, "Foto Realizada!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ctx, getString(R.string.Foto_realizada), Toast.LENGTH_SHORT).show();
                     }
                 }, 3000);
             }
         }
     }
-
 }
-
-
--- app/src/main/java/com/example/josea/puntogestosfoto/Preview.java
+++ app/src/main/java/com/example/josea/puntogestosfoto/Preview.java
@@ -1,12 +1,5 @@
 package com.example.josea.puntogestosfoto;
 
-
-//Documentacion usada:
-// Parte 2.5  y 3 de : http://nuevos-paradigmas-de-interaccion.wikispaces.com/Detecci%C3%B3n+de+patrones+en+Android+-+Gesture+Builder
-//https://github.com/josnidhin/Android-Camera-Example
-//http://nosoandroid.blogspot.com.es/2013/02/retardar-la-ejecucion-de-codigo-durante.html
-
-
 import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
@@ -19,45 +12,92 @@ import android.view.ViewGroup;
 import java.io.IOException;
 import java.util.List;
 
+/// <summary>
+/// Clase que permite usar la camara como pantalla para otra aplicación y manejarla correctamente.
+/// Código sacado por completo de:
+/// https://github.com/josnidhin/Android-Camera-Example
+/// </summary>
 class Preview extends ViewGroup implements SurfaceHolder.Callback {
+
+    /// <summary>
+    /// Estructura usada para los mensajes de depuración.
+    /// </summary>
     private final String TAG = "Preview";
 
+    /// <summary>
+    /// Estructura para la imagen que se mostrará en la aplicación.
+    /// </summary>
     SurfaceView mSurfaceView;
+
+    /// <summary>
+    /// Interfaz para controlar el tamaño de la "superficie"(surfaceview) asignada y el formato; editar píxeles en la "superficie", y monitorizar cambios en la misma.
+    /// </summary>
     SurfaceHolder mHolder;
+
+    /// <summary>
+    /// Estructura para guardar el tamaño que está usando la cámara.
+    /// </summary>
     Size mPreviewSize;
+
+    /// <summary>
+    /// Lista de tamaños que soporta nuestra cámara.
+    /// </summary>
     List<Size> mSupportedPreviewSizes;
+
+    /// <summary>
+    /// Activamos el sensor de la cámara.
+    /// </summary>
     Camera mCamera;
 
+    /// <summary>
+    /// Constructor de la clase, se le asignará lo necesario de la aplicación que use esta clase para poder manejar el surfaceview.
+    /// </summary>
+    /// <param name=" context "> parámetro donde se le pasará el contexto de la aplicación que lo use. </param>
+    /// <param name=" sv "> parámetro donde se le pasará el objeto surfaceview de la aplicación que lo use. </param>
     Preview(Context context, SurfaceView sv) {
         super(context);
 
         mSurfaceView = sv;
-//        addView(mSurfaceView);
-       
         mHolder = mSurfaceView.getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
+    /// <summary>
+    /// Función para asignar la cámara.
+    /// </summary>
+    /// <param name=" camera "> parámetro que se le pasará para asignar la cámara que use la aplicación </param>
     public void setCamera(Camera camera) {
     	mCamera = camera;
+
+        //Comprobamos que la cámara está activa.
     	if (mCamera != null) {
+            // Si está activa se guarda los tamaños de pantalla que soporta en nuestra lista de tamaños
     		mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+
+            // Iniciamos un layout.
     		requestLayout();
 
-    		// get Camera parameters
+    		// Obtenemos los parámetros de la cámara.
     		Camera.Parameters params = mCamera.getParameters();
 
+            // Obtenemos una lista con los modos de enfoque soportados por la cámara
     		List<String> focusModes = params.getSupportedFocusModes();
     		if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
-    			// set the focus mode
+    			// Cambiamos el modo de enfoque
     			params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-    			// set Camera parameters
+    			// Cambiamos los parámetros de la cámara
     			mCamera.setParameters(params);
     		}
     	}
     }
 
+    /// <summary>
+    /// Función para ajustar las dimensiones de la cámara al mostrarla, según la documentación de android encontrada en:
+    // http://developer.android.com/intl/es/reference/android/view/View.html#onMeasure%28int,%20int%29
+    // Measure the view and its content to determine the measured width and the measured height.
+    // This method is invoked by measure(int, int) and should be overridden by subclasses to provide accurate and efficient measurement of their contents.
+    /// </summary>
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         // We purposely disregard child measurements because act as a
@@ -72,6 +112,13 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
         }
     }
 
+    /// <summary>
+    /// Función para asignar los parámetros correctos al layout.
+    /// Según la documentación de android en:
+    /// http://developer.android.com/intl/es/reference/android/view/View.html#onLayout%28boolean,%20int,%20int,%20int,%20int%29
+    /// Called from layout when this view should assign a size and position to each of its children.
+    /// Derived classes with children should override this method and call layout on each of their children.
+    /// </summary>
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (changed && getChildCount() > 0) {
@@ -100,6 +147,10 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
         }
     }
 
+    /// <summary>
+    /// Función necesaria para iniciar el controlar surfaceholder (necesaria al usar el implements SurfaceHolder.Callback)
+    /// </summary>
+    /// <param name=" holder "> parámetro para asignar el controlador </param>
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, acquire the camera and tell it where
         // to draw.
@@ -112,6 +163,10 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
         }
     }
 
+    /// <summary>
+    /// Función necesaria para destruir el controlador igual que la anterior.
+    /// </summary>
+    /// <param name=" holder "> controlador, no se usa pero su cabecera debe ser ésta. </param>
     public void surfaceDestroyed(SurfaceHolder holder) {
         // Surface will be destroyed when we return, so stop the preview.
         if (mCamera != null) {
@@ -119,7 +174,12 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
         }
     }
 
-
+    /// <summary>
+    /// Función usada para obtener las dimensiones optimas, llamada por la función onMeasure.
+    /// </summary>
+    /// <param name=" sizes "> lista con las dimensiones soportadas por el dispositivo </param>
+    /// <param name=" w "> anchura que se obtiene en la función onMeasure usada para calcular la dimensión optima </param>
+    /// <param name=" h "> altura que se obtiene en la función onMeasure usada para calcular la dimensión optima </param>
     private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
         final double ASPECT_TOLERANCE = 0.1;
         double targetRatio = (double) w / h;
@@ -153,6 +213,13 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
         return optimalSize;
     }
 
+    /// <summary>
+    /// Función necesaria para controlar los cambios del controlador surfaceholder (necesaria al usar el implements SurfaceHolder.Callback)
+    /// </summary>
+    /// <param name=" holder "> controlador que se asignará si ha habido cambios en el mismo, no usado pero necesario en la cabecera </param>
+    /// <param name=" format "> parámetro de cambio de formato, no usado pero necesario en la cabecera </param>
+    /// <param name=" w "> parámetro que representa la anchura, no usado pero necesario en la cabecera </param>
+    /// <param name=" h "> parámetro que representa la altura, no usado pero necesario en la cabecera </param>
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
     	if(mCamera != null) {
     		Camera.Parameters parameters = mCamera.getParameters();
@@ -163,5 +230,4 @@ class Preview extends ViewGroup implements SurfaceHolder.Callback {
     		mCamera.startPreview();
     	}
     }
-
 }
